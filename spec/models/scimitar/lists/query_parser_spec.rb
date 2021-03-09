a4d74a32d8bf9b82b6f15050f8f19ac5ad808a90
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2021-03-09 16:24:09 +1300

    Incorporate SCIM Query Filter Parser's tests

--- spec/models/scimitar/lists/query_parser_spec.rb
+++ spec/models/scimitar/lists/query_parser_spec.rb
@@ -16,103 +16,239 @@ RSpec.describe Scimitar::Lists::QueryParser do
   end
 
   # ===========================================================================
-  # PRIVATE METHODS
+  # BASIC PARSING
+  #
+  # Adapted from SCIM Query Filter Parser's non-RSpec tests.
   # ===========================================================================
 
-  context 'internal method' do
+  context 'basic parsing' do
+    it "empty string" do
+      @instance.parse("")
 
-    # =========================================================================
-    # Attributes
-    # =========================================================================
+      rpn = @instance.rpn
+      expect(rpn).to be_empty
 
-    context '#activerecord_attribute' do
-      it 'complains if there is no attribute present' do
-        expect { @instance.send(:activerecord_attribute, nil) }.to raise_error(Scimitar::FilterError)
-        expect { @instance.send(:activerecord_attribute, '' ) }.to raise_error(Scimitar::FilterError)
-      end
+      tree = @instance.tree
+      expect(tree).to be_empty
+    end
 
-      it 'complains if there is no attribute mapping available' do
-        expect { @instance.send(:activerecord_attribute, 'externalId') }.to raise_error(Scimitar::FilterError)
-      end
+    it "user name equals" do
+      @instance.parse(%Q(userName eq "bjensen"))
 
-      it 'returns the attribute' do
-        expect(@instance.send(:activerecord_attribute, 'familyName')).to eql(:last_name)
-      end
-    end # "context '#activerecord_attribute' do"
+      rpn = @instance.rpn
+      expect('userName').to eql(rpn[0])
+      expect('"bjensen"').to eql(rpn[1])
+      expect('eq').to eql(rpn[2])
 
-    # =========================================================================
-    # Operators
-    # =========================================================================
+      tree = @instance.tree
+      expect('eq').to eql(tree[0])
+      expect('userName').to eql(tree[1])
+      expect('"bjensen"').to eql(tree[2])
+    end
 
-    context '#activerecord_operator' do
-      it 'complains if there is no operator present' do
-        expect { @instance.send(:activerecord_operator, nil) }.to raise_error(Scimitar::FilterError)
-        expect { @instance.send(:activerecord_operator, '' ) }.to raise_error(Scimitar::FilterError)
-      end
+    it "family name equals" do
+      @instance.parse(%Q(name.familyName co "O'Malley"))
 
-      it 'complains if the operator is unrecognised' do
-        @instance = described_class.new(MockUser.new.scim_queryable_attributes())
+      rpn = @instance.rpn
+      expect('name.familyName').to eql(rpn[0])
+      expect(%Q("O'Malley")).to eql(rpn[1])
+      expect('co').to eql(rpn[2])
 
-        expect { @instance.send(:activerecord_operator, 'zz') }.to raise_error(Scimitar::FilterError)
-      end
+      tree = @instance.tree
+      expect('co').to eql(tree[0])
+      expect('name.familyName').to eql(tree[1])
+      expect(%Q("O'Malley")).to eql(tree[2])
+    end
 
-      it 'returns expected operators' do
+    it "user name starts with" do
+      @instance.parse(%Q(userName sw "J"))
 
-        # Hand-written instead of using a constant, so we check one set of hand
-        # drawn mappings against another and hopefully increase the chances of
-        # spotting an error.
-        #
-        expected_mapping = {
-          'eq' => '=',
-          'ne' => '!=',
-          'gt' => '>',
-          'ge' => '>=',
-          'lt' => '<',
-          'le' => '<=',
-          'co' => 'LIKE',
-          'sw' => 'LIKE',
-          'ew' => 'LIKE'
-        }
+      rpn = @instance.rpn
+      expect('userName').to eql(rpn[0])
+      expect(%Q("J")).to eql(rpn[1])
+      expect('sw').to eql(rpn[2])
 
-        # Self-check: Is test coverage up to date?
-        #
-        expect(expected_mapping.keys).to match_array(Scimitar::Lists::QueryParser::SQL_COMPARISON_OPERATORS.keys)
+      tree = @instance.tree
+      expect('sw').to eql(tree[0])
+      expect('userName').to eql(tree[1])
+      expect('"J"').to eql(tree[2])
+    end
 
-        expected_mapping.each do | input, expected_output |
-          expect(@instance.send(:activerecord_operator, input)).to eql(expected_output)
-        end
-      end
+    it "title present" do
+      @instance.parse(%Q(title pr))
 
-      it 'is case insensitive' do
-        expect(@instance.send(:activerecord_operator, 'eQ')).to eql('=')
-      end
-    end # "context '#activerecord_operator' do"
+      rpn = @instance.rpn
+      expect('title').to eql(rpn[0])
+      expect('pr').to eql(rpn[1])
 
-    # =========================================================================
-    # Parameters
-    # =========================================================================
+      tree = @instance.tree
+      expect('pr').to eql(tree[0])
+      expect('title').to eql(tree[1])
+    end
 
-    context '#activerecord_parameter' do
-      it 'returns a blank string if a parameter is missing' do
-        expect(@instance.send(:activerecord_parameter, nil )).to eql('')
-        expect(@instance.send(:activerecord_parameter, ''  )).to eql('')
-        expect(@instance.send(:activerecord_parameter, '  ')).to eql('')
-      end
+    it "last modified greater than" do
+      @instance.parse(%Q(meta.lastModified gt "2011-05-13T04:42:34Z"))
 
-      it 'returns the parameter if present' do
-        expect(@instance.send(:activerecord_parameter, 'BAZ')).to eql('BAZ')
-      end
+      rpn = @instance.rpn
+      expect('meta.lastModified').to eql(rpn[0])
+      expect('"2011-05-13T04:42:34Z"').to eql(rpn[1])
+      expect('gt').to eql(rpn[2])
 
-      it 'removes surrounding quotes if present' do
-        expect(@instance.send(:activerecord_parameter, '"BA"Z"')).to eql('BA"Z')
-        expect(@instance.send(:activerecord_parameter, '"BA"Z' )).to eql('"BA"Z')
-        expect(@instance.send(:activerecord_parameter, 'BA"Z"' )).to eql('BA"Z"')
-      end
-    end # "context '#parameter' do"
-  end # "context 'unit tests for internal methods' do"
+      tree = @instance.tree
+      expect('gt').to eql(tree[0])
+      expect('meta.lastModified').to eql(tree[1])
+      expect('"2011-05-13T04:42:34Z"').to eql(tree[2])
+    end
+
+    it "last modified greater than or equal to" do
+      @instance.parse(%Q(meta.lastModified ge "2011-05-13T04:42:34Z"))
+
+      rpn = @instance.rpn
+
+      expect('meta.lastModified').to eql(rpn[0])
+      expect('"2011-05-13T04:42:34Z"').to eql(rpn[1])
+      expect('ge').to eql(rpn[2])
+
+      tree = @instance.tree
+      expect('ge').to eql(tree[0])
+      expect('meta.lastModified').to eql(tree[1])
+      expect('"2011-05-13T04:42:34Z"').to eql(tree[2])
+    end
+
+    it "last modified less than" do
+      @instance.parse(%Q(meta.lastModified lt "2011-05-13T04:42:34Z"))
+
+      rpn = @instance.rpn
+
+      expect('meta.lastModified').to eql(rpn[0])
+      expect('"2011-05-13T04:42:34Z"').to eql(rpn[1])
+      expect('lt').to eql(rpn[2])
+
+      tree = @instance.tree
+      expect('lt').to eql(tree[0])
+      expect('meta.lastModified').to eql(tree[1])
+      expect('"2011-05-13T04:42:34Z"').to eql(tree[2])
+    end
+
+    it "last modified less than or equal to" do
+      @instance.parse(%Q(meta.lastModified le "2011-05-13T04:42:34Z"))
+
+      rpn = @instance.rpn
+
+      expect('meta.lastModified').to eql(rpn[0])
+      expect('"2011-05-13T04:42:34Z"').to eql(rpn[1])
+      expect('le').to eql(rpn[2])
+
+      tree = @instance.tree
+      expect('le').to eql(tree[0])
+      expect('meta.lastModified').to eql(tree[1])
+      expect('"2011-05-13T04:42:34Z"').to eql(tree[2])
+    end
+
+    it "title and user type equal" do
+      @instance.parse(%Q(title pr and userType eq "Employee"))
+
+      rpn = @instance.rpn
+
+      expect('title').to eql(rpn[0])
+      expect('pr').to eql(rpn[1])
+      expect('userType').to eql(rpn[2])
+      expect('"Employee"').to eql(rpn[3])
+      expect('eq').to eql(rpn[4])
+      expect('and').to eql(rpn[5])
+
+      tree = @instance.tree
+      expect(3).to eql(tree.count)
+      expect('and').to eql(tree[0])
+
+      sub = tree[1]
+      expect(2).to eql(sub.count)
+      expect('pr').to eql(sub[0])
+      expect('title').to eql(sub[1])
+
+      sub = tree[2]
+      expect(3).to eql(sub.count)
+      expect('eq').to eql(sub[0])
+      expect('userType').to eql(sub[1])
+      expect('"Employee"').to eql(sub[2])
+    end
+
+    it "title or user type equal" do
+      @instance.parse(%Q(title pr or userType eq "Intern"))
+
+      rpn = @instance.rpn
+
+      expect('title').to eql(rpn[0])
+      expect('pr').to eql(rpn[1])
+      expect('userType').to eql(rpn[2])
+      expect('"Intern"').to eql(rpn[3])
+      expect('eq').to eql(rpn[4])
+      expect('or').to eql(rpn[5])
+
+      tree = @instance.tree
+      expect(3).to eql(tree.count)
+      expect('or').to eql(tree[0])
+
+      sub = tree[1]
+      expect(2).to eql(sub.count)
+      expect('pr').to eql(sub[0])
+      expect('title').to eql(sub[1])
+
+      sub = tree[2]
+      expect(3).to eql(sub.count)
+      expect('eq').to eql(sub[0])
+      expect('userType').to eql(sub[1])
+      expect('"Intern"').to eql(sub[2])
+    end
+
+    it "compound filter" do
+      @instance.parse(%Q{userType eq "Employee" and (emails co "example.com" or emails co "example.org")})
+
+      rpn = @instance.rpn
+
+      expect('userType').to eql(rpn[0])
+      expect('"Employee"').to eql(rpn[1])
+      expect('eq').to eql(rpn[2])
+      expect('emails').to eql(rpn[3])
+      expect('"example.com"').to eql(rpn[4])
+      expect('co').to eql(rpn[5])
+      expect('emails').to eql(rpn[6])
+      expect('"example.org"').to eql(rpn[7])
+      expect('co').to eql(rpn[8])
+      expect('or').to eql(rpn[9])
+      expect('and').to eql(rpn[10])
+
+      tree = @instance.tree
+      expect(3).to eql(tree.count)
+      expect('and').to eql(tree[0])
+
+      sub = tree[1]
+      expect(3).to eql(sub.count)
+      expect('eq').to eql(sub[0])
+      expect('userType').to eql(sub[1])
+      expect('"Employee"').to eql(sub[2])
+
+      sub = tree[2]
+      expect(3).to eql(sub.count)
+      expect('or').to eql(sub[0])
+
+      expect(3).to eql(sub[1].count)
+      expect('co').to eql(sub[1][0])
+      expect('emails').to eql(sub[1][1])
+      expect('"example.com"').to eql(sub[1][2])
+
+      expect(3).to eql(sub[2].count)
+      expect('co').to eql(sub[2][0])
+      expect('emails').to eql(sub[2][1])
+      expect('"example.org"').to eql(sub[2][2])
+    end
+  end # "context 'basic parsing' do"
 
   # ===========================================================================
   # ACTIVERECORD QUERIES
+  #
+  # If you have issues here, check that private method unit tests are passing
+  # before worrying about these higher-level checks.
   # ===========================================================================
 
   context '#to_activerecord_query' do
@@ -288,4 +424,100 @@ RSpec.describe Scimitar::Lists::QueryParser do
       end # "context 'combined AND and OR' do"
     end # "context 'complex cases' do"
   end # "context '#to_activerecord_query' do"
+
+  # ===========================================================================
+  # PRIVATE METHODS
+  # ===========================================================================
+
+  context 'internal method' do
+
+    # =========================================================================
+    # Attributes
+    # =========================================================================
+
+    context '#activerecord_attribute' do
+      it 'complains if there is no attribute present' do
+        expect { @instance.send(:activerecord_attribute, nil) }.to raise_error(Scimitar::FilterError)
+        expect { @instance.send(:activerecord_attribute, '' ) }.to raise_error(Scimitar::FilterError)
+      end
+
+      it 'complains if there is no attribute mapping available' do
+        expect { @instance.send(:activerecord_attribute, 'externalId') }.to raise_error(Scimitar::FilterError)
+      end
+
+      it 'returns the attribute' do
+        expect(@instance.send(:activerecord_attribute, 'familyName')).to eql(:last_name)
+      end
+    end # "context '#activerecord_attribute' do"
+
+    # =========================================================================
+    # Operators
+    # =========================================================================
+
+    context '#activerecord_operator' do
+      it 'complains if there is no operator present' do
+        expect { @instance.send(:activerecord_operator, nil) }.to raise_error(Scimitar::FilterError)
+        expect { @instance.send(:activerecord_operator, '' ) }.to raise_error(Scimitar::FilterError)
+      end
+
+      it 'complains if the operator is unrecognised' do
+        @instance = described_class.new(MockUser.new.scim_queryable_attributes())
+
+        expect { @instance.send(:activerecord_operator, 'zz') }.to raise_error(Scimitar::FilterError)
+      end
+
+      it 'returns expected operators' do
+
+        # Hand-written instead of using a constant, so we check one set of hand
+        # drawn mappings against another and hopefully increase the chances of
+        # spotting an error.
+        #
+        expected_mapping = {
+          'eq' => '=',
+          'ne' => '!=',
+          'gt' => '>',
+          'ge' => '>=',
+          'lt' => '<',
+          'le' => '<=',
+          'co' => 'LIKE',
+          'sw' => 'LIKE',
+          'ew' => 'LIKE'
+        }
+
+        # Self-check: Is test coverage up to date?
+        #
+        expect(expected_mapping.keys).to match_array(Scimitar::Lists::QueryParser::SQL_COMPARISON_OPERATORS.keys)
+
+        expected_mapping.each do | input, expected_output |
+          expect(@instance.send(:activerecord_operator, input)).to eql(expected_output)
+        end
+      end
+
+      it 'is case insensitive' do
+        expect(@instance.send(:activerecord_operator, 'eQ')).to eql('=')
+      end
+    end # "context '#activerecord_operator' do"
+
+    # =========================================================================
+    # Parameters
+    # =========================================================================
+
+    context '#activerecord_parameter' do
+      it 'returns a blank string if a parameter is missing' do
+        expect(@instance.send(:activerecord_parameter, nil )).to eql('')
+        expect(@instance.send(:activerecord_parameter, ''  )).to eql('')
+        expect(@instance.send(:activerecord_parameter, '  ')).to eql('')
+      end
+
+      it 'returns the parameter if present' do
+        expect(@instance.send(:activerecord_parameter, 'BAZ')).to eql('BAZ')
+      end
+
+      it 'removes surrounding quotes if present' do
+        expect(@instance.send(:activerecord_parameter, '"BA"Z"')).to eql('BA"Z')
+        expect(@instance.send(:activerecord_parameter, '"BA"Z' )).to eql('"BA"Z')
+        expect(@instance.send(:activerecord_parameter, 'BA"Z"' )).to eql('BA"Z"')
+      end
+    end # "context '#parameter' do"
+  end # "context 'unit tests for internal methods' do"
 end # "RSpec.describe Scimitar::Lists::QueryParser do"
