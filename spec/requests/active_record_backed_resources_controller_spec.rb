767cd627bff47b3e4042eca653130c4dc8728fd2
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2021-03-15 09:17:14 +1300

    More tests

--- spec/requests/active_record_backed_resources_controller_spec.rb
+++ spec/requests/active_record_backed_resources_controller_spec.rb
@@ -17,53 +17,85 @@ RSpec.describe Scimitar::ActiveRecordBackedResourcesController do
       expect(result['itemsPerPage']).to eql(100)
     end
 
-    it 'with items' do
-      u1 = MockUser.create(username: '1')
-      u2 = MockUser.create(username: '2')
-      u3 = MockUser.create(username: '3')
+    context 'with items' do
+      before :each do
+        @u1 = MockUser.create(username: '1', first_name: 'Foo', last_name: 'Ark', home_email_address: 'home_1@test.com')
+        @u2 = MockUser.create(username: '2', first_name: 'Foo', last_name: 'Bar', home_email_address: 'home_2@test.com')
+        @u3 = MockUser.create(username: '3', first_name: 'Foo',                   home_email_address: 'home_3@test.com')
+      end
 
-      get '/Users', params: { format: :scim }
+      it 'without parameters' do
+        get '/Users', params: { format: :scim }
 
-      expect(response.status).to eql(200)
-      result = JSON.parse(response.body)
+        expect(response.status).to eql(200)
+        result = JSON.parse(response.body)
 
-      expect(result['totalResults']).to eql(3)
-      expect(result['Resources'].size).to eql(3)
+        expect(result['totalResults']).to eql(3)
+        expect(result['Resources'].size).to eql(3)
 
-      ids = result['Resources'].map { |resource| resource['id'] }
-      expect(ids).to match_array([u1.id.to_s, u2.id.to_s, u3.id.to_s])
+        ids = result['Resources'].map { |resource| resource['id'] }
+        expect(ids).to match_array([@u1.id.to_s, @u2.id.to_s, @u3.id.to_s])
 
-      usernames = result['Resources'].map { |resource| resource['userName'] }
-      expect(usernames).to match_array(['1', '2', '3'])
-    end
+        usernames = result['Resources'].map { |resource| resource['userName'] }
+        expect(usernames).to match_array(['1', '2', '3'])
+      end
 
-    it 'with a filter' do
-      u1 = MockUser.create(first_name: 'Foo', last_name: 'Bar', home_email_address: 'home_1@test.com')
-      u2 = MockUser.create(first_name: 'Foo', last_name: 'Bar', home_email_address: 'home_2@test.com')
-      u3 = MockUser.create(first_name: 'Foo',                   home_email_address: 'home_3@test.com')
+      it 'with a filter' do
+        get '/Users', params: {
+          format: :scim,
+          filter: 'name.givenName eq "Foo" and name.familyName pr and emails ne "home_1@test.com"'
+        }
 
-      get '/Users', params: {
-        format: :scim,
-        filter: 'name.givenName eq "Foo" and name.familyName pr'# and emails ne "home_1@test.com"'
-      }
-      byebug
-      expect(response.status).to eql(200)
-      result = JSON.parse(response.body)
+        expect(response.status).to eql(200)
+        result = JSON.parse(response.body)
 
-      expect(result['totalResults']).to eql(3)
-      expect(result['Resources'].size).to eql(3)
+        expect(result['totalResults']).to eql(1)
+        expect(result['Resources'].size).to eql(1)
 
-      ids = result['Resources'].map { |resource| resource['id'] }
-      expect(ids).to match_array([u1.id.to_s, u2.id.to_s, u3.id.to_s])
+        ids = result['Resources'].map { |resource| resource['id'] }
+        expect(ids).to match_array([@u2.id.to_s])
 
-      usernames = result['Resources'].map { |resource| resource['userName'] }
-      expect(usernames).to match_array(['1', '2', '3'])
-    end
+        usernames = result['Resources'].map { |resource| resource['userName'] }
+        expect(usernames).to match_array(['2'])
+      end
 
-    it 'pagination' do
-    end
+      it 'pagination' do
+        get '/Users', params: {
+          format: :scim,
+          count:  2
+        }
 
-    it 'offsets' do
-    end
-  end
+        expect(response.status).to eql(200)
+        result = JSON.parse(response.body)
+
+        expect(result['totalResults']).to eql(2)
+        expect(result['Resources'].size).to eql(2)
+
+        ids = result['Resources'].map { |resource| resource['id'] }
+        expect(ids).to match_array([@u1.id.to_s, @u2.id.to_s])
+
+        usernames = result['Resources'].map { |resource| resource['userName'] }
+        expect(usernames).to match_array(['1', '2'])
+      end
+
+      it 'offsets' do
+        get '/Users', params: {
+          format:    :scim,
+          startIndex: 2
+        }
+
+        expect(response.status).to eql(200)
+        result = JSON.parse(response.body)
+
+        expect(result['totalResults']).to eql(2)
+        expect(result['Resources'].size).to eql(2)
+
+        ids = result['Resources'].map { |resource| resource['id'] }
+        expect(ids).to match_array([@u2.id.to_s, @u3.id.to_s])
+
+        usernames = result['Resources'].map { |resource| resource['userName'] }
+        expect(usernames).to match_array(['2', '3'])
+      end
+    end # "context 'with items' do"
+  end # "context 'lists' do"
 end
