a0359e5d17d07f1fcf905d84df5890f458f25aba
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2021-03-24 08:57:28 +1300

    Improve example code

--- spec/apps/dummy/app/models/mock_group.rb
+++ spec/apps/dummy/app/models/mock_group.rb
@@ -56,7 +56,14 @@ class MockGroup < ActiveRecord::Base
           id   = scim_list_entry['value']
           type = scim_list_entry['type' ] || 'User'
 
-          type&.downcase == 'user' ? MockUser.find_by_id(id) : MockGroup.find_by_id(id)
+          case type.downcase
+            when 'user'
+              MockUser.find_by_id(id)
+            when 'group'
+              MockGroup.find_by_id(id)
+          else
+            raise Scimitar::InvalidSyntaxError.new("Unrecognised type #{type.inspect}")
+          end
         }
       ]
     }
