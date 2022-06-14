afd852f5ef982b0591b3201d4b28be60b5a7d34e
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2022-06-15 10:45:40 +1200

    Version 2.0.2 and maintenance 'bundle update'

--- lib/scimitar/version.rb
+++ lib/scimitar/version.rb
@@ -3,11 +3,11 @@ module Scimitar
   # Gem version. If this changes, be sure to re-run "bundle install" or
   # "bundle update".
   #
-  VERSION = '2.0.1'
+  VERSION = '2.0.2'
 
   # Date for VERSION. If this changes, be sure to re-run "bundle install"
   # or "bundle update".
   #
-  DATE = '2022-04-20'
+  DATE = '2022-06-15'
 
 end
