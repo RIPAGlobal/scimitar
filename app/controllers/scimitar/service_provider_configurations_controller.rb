ecd5b4477e1a3151a1a809561fde68b429c669d3
Peter Bouda <pbouda@outlook.com>
2024-10-31 10:38:43 +0000

    Remove uses_defaults from ServiceProviderConfig endpoint

--- app/controllers/scimitar/service_provider_configurations_controller.rb
+++ app/controllers/scimitar/service_provider_configurations_controller.rb
@@ -1,7 +1,9 @@
 module Scimitar
   class ServiceProviderConfigurationsController < ApplicationController
     def show
-      render json: Scimitar.service_provider_configuration(location: request.url)
+      service_provider_configuration = Scimitar.service_provider_configuration(location: request.url).as_json
+      service_provider_configuration.delete("uses_defaults")
+      render json: service_provider_configuration
     end
   end
 end
